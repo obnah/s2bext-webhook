1c2a4459d2e64416611846a3b1d204ef95544b63
Aaron Han <aaronhan@juniper.net>
2017-08-31 23:52:00 -0700

    add seq

--- s2bext-wrapper.sh
+++ s2bext-wrapper.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+# =====================================================
+pr=$1
+cmd=$2
+seq=$3
 
 
-echo $1
-echo $2
+shopt -s expand_aliases
+source ~/.bashrc
+source ~aaronhan/.s2bext/common
 
-$2
+
+if [[ -n $pr ]]; then 
+	if valid_pr_number $pr; then
+		mkdir -p ~/$pr
+		cd ~/$pr
+	else
+		echo "Invalid PR Number"
+	fi
+fi
+
+s2bext $cmd
+
+echo ""
+echo "''''''"
+echo "SEQ $seq"
